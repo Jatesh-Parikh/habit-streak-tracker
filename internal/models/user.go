7162e1e83980344d1cf6c24231783bd07aa746d9
Jatesh Parikh <jateshparikh97@gmail.com>
2026-09-19 13:45:46 +0530

    add models/user

---
+++ internal/models/user.go
@@ -0,0 +1,22 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        string    `json:"id" db:"id"`
+	Name      string    `json:"name" db:"name"`
+	Email     string    `json:"email" db:"email"`
+	Password  string    `json:"password" db:"password"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+type UserInput struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
+type AuthPayload struct {
+	Token string `json:"token"`
+	User  *User  `json:"user"`
+}
