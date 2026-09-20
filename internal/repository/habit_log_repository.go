6b1a71e1c78f9637fa4b92affa13341a78d46e70
Jatesh Parikh <jateshparikh97@gmail.com>
2026-09-20 22:57:37 +0530

    add habit_log_repository/check_duplicate_log

--- internal/repository/habit_log_repository.go
+++ internal/repository/habit_log_repository.go
@@ -1,1 +1,32 @@
 package repository
+
+import (
+	// "habit-streak-tracker/internal/models"
+	"database/sql"
+	// "errors"
+	"fmt"
+	// "strings"
+	"time"
+)
+
+type HabitLogRepository struct {
+	DB *sql.DB
+}
+
+func NewHabitLogRepository(db *sql.DB) *HabitLogRepository {
+	return &HabitLogRepository{DB: db}
+}
+
+func (r *HabitLogRepository) CheckDuplicateLog(habitID string, completedDate time.Time) (bool, error) {
+	dateStr := completedDate.Format("2006-01-02")
+
+	var count int
+
+	err := r.DB.QueryRow("SELECT COUNT(*) FROM habit_logs WHERE habit_id = ? AND completed_date = ?", habitID, dateStr).Scan(&count)
+
+	if err != nil {
+		return false, fmt.Errorf("Failed to check duplicate log: %w", err)
+	}
+
+	return count > 0, nil
+}
