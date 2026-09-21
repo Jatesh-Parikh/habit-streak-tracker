af7db7bd014436a15dea6807c5d2d548dab730a2
Jatesh Parikh <jateshparikh97@gmail.com>
2026-09-21 23:17:35 +0530

    add internal/graph/resolver

--- internal/graph/resolvers/resolver.go
+++ internal/graph/resolvers/resolver.go
@@ -1,8 +1,14 @@
 package resolvers
 
+import "habit-streak-tracker/internal/repository"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require
 // here.
 
-type Resolver struct{}
+type Resolver struct {
+	UserRepo     *repository.UserRepository
+	HabitRepo    *repository.HabitRepository
+	HabitLogRepo *repository.HabitLogRepository
+}
