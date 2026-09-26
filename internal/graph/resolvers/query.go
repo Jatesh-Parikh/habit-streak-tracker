3494124302f58ba50e4d122729fbe1aaf4178ebe
Jatesh Parikh <jateshparikh97@gmail.com>
2026-09-26 21:58:04 +0530

    add graph/resolvers/query/Habits

--- internal/graph/resolvers/query.go
+++ internal/graph/resolvers/query.go
@@ -22,3 +22,19 @@ func (r *queryResolver) Me(ctx context.Context) (*models.User, error) {
 
 	return user, nil
 }
+
+func (r *queryResolver) Habits(ctx context.Context) ([]*models.Habit, error) {
+	userID, ok := middleware.GetUserID(ctx)
+
+	if !ok {
+		return nil, fmt.Errorf("Unauthorized")
+	}
+
+	habits, err := r.HabitRepo.GetHabitsByUserID(userID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return habits, nil
+}
